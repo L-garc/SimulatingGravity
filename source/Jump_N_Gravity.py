6a5134771ce1798c0f772e5370158899d63aecde
L-garc <95556354+L-garc@users.noreply.github.com>
2022-02-05 13:20:26 -0800

    Elevated Ground
    Added capability to blit ground sections which are higher than the "base" ground  object section.

--- source/Jump_N_Gravity.py
+++ source/Jump_N_Gravity.py
@@ -13,13 +13,17 @@ clock = pygame.time.Clock()
 #Defines colors using rgb values
 black = (0, 0, 0)
 white = (255, 255, 255)
+red = (255, 0, 0)
+green = (0, 255, 0)
 
 gravity = -9.8 #Gravitational acceleration is negative
 initVel = 14 #Inital velocity is positive
+ratio = 45 #Sets the pixel to meter ratio (calculated as pixels per meter)
+speed = 5 #Horizontal movement speed in meters per second
 
 HeldIntervals = [] #Holds every delta y in the upward portion of the jump parabola
-FallList = HeldIntervals #Holds the reversed list of delta y's
-FallList.reverse() #Reverses the order of the delta y's for the downward portion of the jump parabola
+difVals = []
+FallList = difVals #Will hold the reversed list of delta y's, for the downward portion
 
 '''
 Equation for the height of an object in terms of time since it was launched
@@ -27,14 +31,25 @@ H(x) = -9.8(x**2) + bx + c
 Height (after x seconds) = acceleration due to gravity + initial velocity of launched object + initial height (off the ground)
 If there were other sources of acceleration (such as a gravitational feild of another object overhead) then you would add acceleration due to that object
 '''
+class Ground():
+    def __init__(self, color, width, height, xpos, ypos):
+        self.color = color
+        self.w = width
+        self.h = height
+        self.x = xpos
+        self.y = ypos
+        self.Rec = pygame.Rect(self.x, self.y, self.w, self.h)
 
+    def blit(self):
+        pygame.draw.rect(ds, self.color, self.Rec)
+        
 class Player():
     def __init__(self):
         self.color = black
         self.w = 20
         self.h = 20
         self.x = 20
-        self.y = 700
+        self.y = 680
         self.jumping = False
         self.falling = False
         self.grounded = True
@@ -53,16 +68,16 @@ class Player():
 
     def fall(self):
         if self.jumping: #If Space bar is pressed
-            dH = HeldIntervals[self.index]
+            dH = difVals[self.index] * ratio #Calculate the height the box should be at, then apply pixel : meter ratio
             self.Rec = self.Rec.move(0, -dH)
-            if (self.index < len(HeldIntervals)-1):
+            if (self.index < len(difVals)-1):
                 self.index += 1
             else:                       #After iterating through HeldIntervals
                 self.jumping = False
                 self.falling = True
                 self.index = 1          #Starts at 1 not 0 because otherwise the box would be at its apex for 2 frames rather than 1
         elif self.falling:
-            dH = FallList[self.index]
+            dH = FallList[self.index] * ratio
             self.Rec = self.Rec.move(0, dH)
             if (self.index < len(FallList)-1):
                 self.index += 1
@@ -71,13 +86,22 @@ class Player():
                 self.grounded = True
         else:
             pass
+       
+#Anything we want to draw to screen call here
+#For the box to appear "In front" of raised grounds, blit it after the other ground objects.
+#To hide a box behind a ground object, blit it before the ground object
+def blit2screen():
+    baseGround.blit()
+    elevG1.blit()
+    player.blit()
+    
 #The equation for height of a projectile using gravity and a predetermined initial velocity (t refers to time in seconds)
 def H(t):
     return ((gravity * (t**2)) + (initVel * t))
 
 def findIntervals():
     apex = -initVel / (2 * gravity) #Calculates at what time the maximum height occurs (Only works for gravity and intial velocity, no other forces)
-    index = 1 #Don't need height at time zero, height is the objects current position at t=0
+    index = 0
     calc = True
     
     while calc:
@@ -90,10 +114,21 @@ def findIntervals():
         else:
             calc = False
 
-findIntervals() #Pre-calculates height of projectiles at certain intervals which line up with program frames
-player = Player() #Creates instance of Player class
+    for i in range(0,len(HeldIntervals)-1):
+        difVals.append( HeldIntervals[i+1] - HeldIntervals[i])
 
-dx = 0
+    
+     #FallList is defined by difVals, since we populate difVals here, and we want FallList reversed, it must be done here
+            #==========================================================================================================================================================================
+
+findIntervals() #Pre-calculates height of projectiles at certain intervals which line up with program frames
+FallList = difVals[::-1] #This method of reversing a list prevents the original "difVals" from also being reversed, reverse() reverses both
+
+player = Player() #Creates instance of Player class
+baseGround = Ground(green, 1280, 20, 0, 700) #Creates instance of Ground class
+elevG1 = Ground(green, 100, 250, 1180, 470)
+
+dx = 0 #Initialize horizontal movement to 0 (no change in x-position)
 while True:
     for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -104,9 +139,9 @@ while True:
                     if player.grounded:
                         player.jump()
                 elif event.key == pygame.K_LEFT:
-                    dx = -5 
+                    dx = -speed * (ratio / 60) #Divide by 60 because fps is limited to 60, position is updated every 60th of a second
                 elif event.key == pygame.K_RIGHT:
-                    dx = 5
+                    dx = speed * (ratio / 60)
             if event.type == pygame.KEYUP:
                 if ((event.key == pygame.K_LEFT) or (event.key == pygame.K_RIGHT)):
                     dx = 0
@@ -114,7 +149,7 @@ while True:
     player.fall() #Brings the box down unless box attribute grounded is true
     
     ds.fill(white) #White background
-    player.blit() #Draw the box on the screen
+    blit2screen() #Draw objects on the screen
 
     pygame.display.flip() #Update screen
     clock.tick(60) #Limit frame rate to 60 fps (realworld 59 - 62 fps)
